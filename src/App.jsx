4664837c944a50d50f602b804d3bc866303ca175
mehedi hasan <mehedihasan.j135@gmail.com>
2026-03-31 10:27:37 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <h1>hello</h1>
+    </>
+  );
+}
+
+export default App;
